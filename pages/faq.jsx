5bdb173f25f02c53f8d06e6db9585cd9ec2f7513
Santlago <breno.santiago.8@gmail.com>
2024-02-02 15:44:11 -0300

    adicionando faq page

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,43 +1,22 @@
-import Title from '../src/components/Title'
-import Head from 'next/head'
-import Link from 'next/link'
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
-export async function getServerSideProps() {
-    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+export default FAQScreen; 
+
+export async function getStaticProps() {
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
         .then((respostaDoServidor) => {
-            return respostaDoServidor.json()
+            return respostaDoServidor.json();
         })
         .then((resposta) => {
-            return(resposta)
-        })
-    return {
-        props: {
-            faq
-        },
-    }
-}
+            return resposta;
+        });
 
-export default function FAQPage({ faq }) {
-    return(
-        <div>
-            <Title>
-                FAQ - Alura Cases Campanha
-            </Title>
-            <h1>Alura Cases - FAQ</h1>
-            <Link href="/">
-                Ir para home
-            </Link>
-            <ul>
-                {faq.map(({ answer, question }) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+    return {
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
