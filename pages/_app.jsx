96b700d394ae68d886439ea0dfc92dd3863b3614
Santlago <breno.santiago.8@gmail.com>
2024-02-02 16:01:38 -0300

    adicionando google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,19 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Title>
+                {/* <!-- Google tag (gtag.js) --> */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-0039FXRHXW"></script>
+                <script 
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+        
+                            gtag('config', 'G-0039FXRHXW');
+                        `
+                    }}
+                />
                 {/* Tag do google analytics */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com"  />
